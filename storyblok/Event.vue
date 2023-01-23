5bcbfdb6d3a867dc487bc373072385213cb08f53
markgrahamdawson <markgrahamdawson@gmail.com>
2023-01-23 11:23:21 +0000

    hotfix style changes

--- storyblok/Event.vue
+++ storyblok/Event.vue
@@ -1,40 +1,44 @@
 <template>
-  <div v-editable="blok" class="p-6 w-full">
-    <div class="font-josan px-10">
-      <NuxtLink to="/events">Back to events</NuxtLink>
-      <div class="relative flex justify-center">
-        <img :src="blok.thumbnail?.filename + '/m/400x0' || 'https://picsum.photos/300/175'" :alt="blok.image?.alt"
-          class="" />
-      </div>
-      <div class="px-16 pb-4 pt-8 font-semibold flex">
-        <div class="w-3/4 text-teal">
-          {{ blok.date || "Date" }}
+  <div class="mx-auto sm:block max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div v-editable="blok" class="p-6 w-full">
+      <div class="font-josan px-10">
+        <div class="flex ml-4">
+          <IconsChevronLeft/>
+          <NuxtLink to="/events" class="font-permark ml-2">Back to events</NuxtLink>
         </div>
-        <!-- <div class="w-1/4">
-          <div v-if="!firebaseUser" class="">
-            <NuxtLink to="/firebase">Sign in to buy tickets for this event!</NuxtLink>
+        <div class="relative flex justify-center">
+          <img :src="blok.thumbnail?.filename + '/m/400x0' || 'https://picsum.photos/300/175'" :alt="blok.image?.alt"
+            class="" />
+        </div>
+        <div class="px-16 pb-4 pt-8 font-semibold flex">
+          <div class="w-3/4 text-teal">
+            {{ blok.date || "Date" }}
           </div>
-          <div v-else>
-            <button class="border border-black" @click="createSub"
-              :disabled="!products[0]?.prices[0]?.unit_amount || isLoading">
-              <div>{{ isLoading ? "Loading..." : "Buy tickets for this event" }}</div>
-              <div v-if="products[0]?.prices[0]?.unit_amount">£ {{ products[0]?.prices[0]?.unit_amount / 100 }}</div>
-            </button>
-          </div>
-        </div> -->
-      </div>
-      <div class="w-full px-16 py-2 font-permark ">
-        {{ blok.performance || "Performance" }}
-      </div>
-      <div class="w-full px-16 py-2">
-        {{ blok.company || "Company" }}
-      </div>
-      <div class="w-full px-16">
-        {{ blok.content || "Lorem Ipsum" }}
+          <!-- <div class="w-1/4">
+            <div v-if="!firebaseUser" class="">
+              <NuxtLink to="/firebase">Sign in to buy tickets for this event!</NuxtLink>
+            </div>
+            <div v-else>
+              <button class="border border-black" @click="createSub"
+                :disabled="!products[0]?.prices[0]?.unit_amount || isLoading">
+                <div>{{ isLoading ? "Loading..." : "Buy tickets for this event" }}</div>
+                <div v-if="products[0]?.prices[0]?.unit_amount">£ {{ products[0]?.prices[0]?.unit_amount / 100 }}</div>
+              </button>
+            </div>
+          </div> -->
+        </div>
+        <div class="w-full px-16 py-2 font-permark ">
+          {{ blok.performance || "Performance" }}
+        </div>
+        <div class="w-full px-16 py-2">
+          {{ blok.company || "Company" }}
+        </div>
+        <div class="w-full px-16">
+          {{ blok.content || "Lorem Ipsum" }}
+        </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <script lang="ts" setup>
