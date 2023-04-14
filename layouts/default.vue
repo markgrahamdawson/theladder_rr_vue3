4b97a66431c6f296fbbc3a8fab6b46d55fa78ad7
markgrahamdawson <markgrahamdawson@gmail.com>
2023-04-14 08:29:00 +0100

    built out header component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,7 @@
 <template>
   <div class="min-h-screen bg-offwhite">
-    <Header />
-    <!-- <div class="bg-[url('~/assets/scatter-background.svg')] bg-auto bg-no-repeat w-full"> -->
-      <div class="relative bg-[url('~/assets/scatter-background-left.svg')] bg-auto bg-no-repeat w-full">
-        <!-- <div class="absolute border border-red-500 bg-[url('~/assets/radio.svg')] bg-auto bg-no-repeat w-full"> -->
-          <!-- abc where is this??? -->
-          <!-- <div class="border-red-500 bg-[url('~/assets/scatter-background-left.svg')] bg-auto bg-no-repeat w-full"> -->
-          <!-- <div class="border border-red-500"> -->
-            <!-- abc where is this??? -->
-            <!-- <div class="border-red-500 bg-[url('~/assets/scatter-background-right.svg')] bg-auto bg-no-repeat w-full"> -->
-              <!-- abc where is this??? -->
-              <!-- <div class="border-red-500 bg-[url('~/assets/scatter-background-rightv2.svg')] bg-auto bg-no-repeat w-full"> -->
-              <slot></slot>
-              <!-- </div> -->
-              <!-- abc where is this??? -->
-            <!-- </div> -->
-            <!-- abc where is this??? -->
-          <!-- </div> -->
-          <!-- abc where is this??? -->
-        <!-- </div> -->
-      </div>
-    <!-- </div> -->
+    <HeaderNew />
+    <slot></slot>
   </div>
 </template>
 
