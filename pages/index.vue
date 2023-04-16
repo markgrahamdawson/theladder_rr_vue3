9898559715be65be0896bf30fc8e75b7074e9469
markgrahamdawson <markgrahamdawson@gmail.com>
2023-04-16 17:16:56 +0100

    post merge tidy up

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,8 @@
 <template>
-  <StoryblokComponent v-if="story" :blok="story.content"></StoryblokComponent>
-  <div class="mx-auto sm:block max-w-7xl px-2 sm:px-6 lg:px-8">
-  <div class="min-h-screen sm:p-16 flex ">
-    <div class="px-10 pb-10">
-      <div class="h-2/3 w-full flex items-center justify-center hidden md:block">
-      </div>
-      <div class=" w-full pt-8 md:hidden">
-        <div class="w-full flex items-center justify-center">
-          <img src="~/assets/passmoreedwardslibrary.jpeg" alt="The Ladder Logo" class="h-1/3 object-cover"  />
-        </div>
-      </div>
-    </div>
+  <div>
+    <HomeAbout />
+    <StoryblokComponent v-if="story" :blok="story.content"></StoryblokComponent>
   </div>
-</div>
 </template>
 
 <script lang="ts" setup>
