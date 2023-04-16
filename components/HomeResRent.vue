8ffc0c94068f84d263754ffc23b4b98a508c2999
markgrahamdawson <markgrahamdawson@gmail.com>
2023-04-16 22:38:27 +0100

    added residency rent home component

---
+++ components/HomeResRent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="md:flex space-y-2 md:space-y-0 md:space-x-2 font-josan text-white md:h-[500px]">
+    <div class="md:w-1/2 bg-[url('~/assets/residency.png')] bg-auto bg-no-repeat bg-center p-20 space-y-5">
+      <div class="text-white text-3xl">
+        Artists residency
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias rem veritatis dolorem adipisci, numquam facere! Itaque dolorem neque earum ut, sint assumenda, nihil corporis, temporibus sed laborum repellendus dolorum rerum.
+      </div>
+      <div class="flex space-x-4"><div>Check it out</div><IconsArrow class=""/></div>
+      <div class="flex justify-center">
+        <IconsSnake class="md:hidden stroke-white h-32 w-32 fill-white"/>
+      </div>
+    </div>
+    <div class="md:w-1/2 bg-[url('~/assets/rent.png')] bg-auto bg-no-repeat bg-center p-20 space-y-5">
+      <div class="text-white text-3xl">
+        Rent a practise room
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias rem veritatis dolorem adipisci, numquam facere! Itaque dolorem neque earum ut, sint assumenda, nihil corporis, temporibus sed laborum repellendus dolorum rerum.
+      </div>
+      <div class="flex space-x-4"><div>Check it out</div><IconsArrow class=""/></div>
+      <div class="flex justify-center">
+        <IconsEyeInky class=" md:hidden text-white h-32 w-32 fill-white"/>
+      </div>
+    </div>
+  </div>
+</template>
