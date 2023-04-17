ea33fd9dd60b290bf631c3de5041e1982c642d22
markgrahamdawson <markgrahamdawson@gmail.com>
2023-04-17 21:21:38 +0100

    added homepage cafe component

---
+++ components/HomeCafe.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex py-10">
+    <!-- left box -->
+    <div class="w-1/2 m-auto">
+      <img src="~/assets/Cafe.png" alt="Beets and Roots Cafe" />
+    </div>
+    <!-- right box -->
+    <div class="w-1/2 m-auto bg-offwhite font-josefin my-4 p-10 space-y-4 relative">
+      <div class="font-semibold text-4xl">
+        <h1>Beets and Roots</h1> 
+      </div>
+      <div>
+        <div class="font-semibold text-xl pb-2 uppercase">Now open</div> 
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        </div> 
+      </div>
+      <div>
+        <div class="font-semibold text-xl pb-2 uppercase">Opening times</div> 
+        <div>
+          Thursday 12-6
+        </div>
+        <div>
+          Friday 12-6
+        </div> 
+        <div>
+          Saturday 12-6
+        </div> 
+      </div>
+      <IconsSquigles class="hidden md:block absolute h-44 w-44 lg:h-64 lg:w-64 right-0 -bottom-10"/>
+    </div>
+  </div>
+</template>
