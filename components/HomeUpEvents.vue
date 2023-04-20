37edd2d7893b6f9493894255b58c24ddc5296624
Richie <imarichicken@gmail.com>
2023-04-19 20:08:54 -0700

    Built out Upcoming Events component

---
+++ components/HomeUpEvents.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="py-5">
+      <div class="md:grid md:grid-cols-[6fr_10fr] items-center w-full flex flex-col-reverse">
+    <!-- Left Col -->
+      <div class="bg-black flex items-center px-10 py-10 h-full w-full text-center">
+        <div class="text-white font-josefin mx-auto space-y-5">
+          <div class="uppercase text-4xl font-semibold">
+            Event Title
+          </div>
+          <div class="font-semibold">
+            12th January 8pm-10pm
+          </div>
+          <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, voluptas!
+          </div>
+          <div>
+            <button class="bg-orange text-white font-semibold rounded-md px-5 py-2 uppercase">
+              Book Now
+            </button>
+          </div>
+          
+        </div>
+    </div>
+    <!-- Right Col -->
+      <div>
+        <img src="~/assets/events.png" class="md:w-full object-cover object-center" alt="Kids sitting a circle scribbling on paper."/>
+      </div>
+    </div>
+  </div>
+  </template>
