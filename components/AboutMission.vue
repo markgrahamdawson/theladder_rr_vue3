7af80edd9c64472e6f8632020b9fc0b4f7dd5e40
Richie <imarichicken@gmail.com>
2023-04-30 16:36:19 -0700

    AboutMission components (almost) complete

---
+++ components/AboutMission.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class=" bg-white md:grid md:grid-cols-2 items-center w-full">
+        <!-- Left Col -->
+        <div class="bg-offwhite text-left px-12 py-20">
+            <h1 class="title font-josefin font-semibold text-4xl title pb-4">Mission</h1> 
+            <div class="font-semibold">
+                We are building a creative space for Redruth and Cornwall. This will include space for creatives to work including studios and co-working but also for community events and activities including fance classes, children and young people activities, fitness and music. Every two years all our residents will come together as part of a festival.
+            </div>
+        </div>
+        <!-- Right Col -->
+        <div class="items-end justify-center">
+            <img src="~/assets/libraryOutside.png" alt="Josh and Felix talking by the Redruth library window" class="h-1/5 object-cover" />
+            <IconsBuilding class="hidden md:block fill-white absolute h-40 w-40 right-[70px] bottom-[130px]"/>
+        </div>
+    </div>  
+</template>
+  
