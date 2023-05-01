0a56188cf3ec8bd128d568c8cea06d3dd661ff2f
Richie <imarichicken@gmail.com>
2023-04-30 17:33:09 -0700

    Completed AboutHero component

--- components/AboutHero.vue
+++ components/AboutHero.vue
@@ -1,4 +1,8 @@
 <template>
-    <img src="~/assets/abouthero.png" alt="A solid orange color with rough scratches" 
-    class="md:28 lg:h-[450px] w-full object-cover	"  />
+    <!-- <img src="~/assets/abouthero.png" alt="A solid orange color with rough scratches" 
+    class="md:28 lg:h-[450px] w-full object-cover	"  /> -->
+
+  <div class="md:w-full flex bg-[url('~/assets/heroTexture.png')] bg-cover bg-no-repeat bg-center  justify-center items-center font-josefin text-white font-bold py-10 sm:text-[5vw] uppercase">
+    ABOUT
+  </div>  
   </template>
