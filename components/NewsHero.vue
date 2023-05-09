3803745dac659f28054e4549d03e8635a34350b4
Richie <imarichicken@gmail.com>
2023-05-09 15:58:57 -0700

    Created NewsHero component

---
+++ components/NewsHero.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="md:w-full flex bg-[url('~/assets/heroTexture.png')] bg-cover bg-no-repeat bg-center  justify-center items-center font-josefin text-white font-bold py-10 sm:text-[5vw] uppercase">
+    NEWS
+  </div>  
+  </template>
