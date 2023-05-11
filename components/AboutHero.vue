8fa6a4e38452a3ab6e8733cefeff6709f2dbdc5e
markgrahamdawson <markgrahamdawson@gmail.com>
2023-05-11 20:08:38 +0100

    edits to text size

--- components/AboutHero.vue
+++ components/AboutHero.vue
@@ -1,8 +1,10 @@
 <template>
-    <!-- <img src="~/assets/abouthero.png" alt="A solid orange color with rough scratches" 
+  <!-- <img src="~/assets/abouthero.png" alt="A solid orange color with rough scratches" 
     class="md:28 lg:h-[450px] w-full object-cover	"  /> -->
 
-  <div class="md:w-full flex bg-[url('~/assets/heroTexture.png')] bg-cover bg-no-repeat bg-center  justify-center items-center font-josefin text-white font-bold py-10 sm:text-[5vw] uppercase">
-    ABOUT
-  </div>  
-  </template>
+  <div
+    class="md:w-full flex bg-[url('~/assets/heroTexture.png')] bg-cover bg-no-repeat bg-center justify-center items-center font-josefin text-white font-bold uppercase"
+  >
+    <div class="text-3xl sm:text-6xl p-10">ABOUT</div>
+  </div>
+</template>
