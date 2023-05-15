2b320c7e527ff02931909904aa0e6b22990aa0cd
markgrahamdawson <markgrahamdawson@gmail.com>
2023-05-15 22:03:38 +0100

    added dropdown for about on nav

--- components/Hero.vue
+++ components/Hero.vue
@@ -5,6 +5,12 @@
   <div
     class="md:w-full flex bg-[url('~/assets/heroTexture.png')] bg-cover bg-no-repeat bg-center justify-center items-center font-josefin text-white font-bold uppercase"
   >
-    <div class="text-3xl sm:text-6xl p-10">ABOUT</div>
+    <div class="text-3xl sm:text-6xl p-10">{{ props.text }}</div>
   </div>
 </template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  text: { type: String, required: true },
+});
+</script>
