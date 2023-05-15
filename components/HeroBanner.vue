690de209f78a26601cb5be122ee45a1f3028ec10
Richie <imarichicken@gmail.com>
2023-05-14 19:43:31 -0700

    Created Generic Hero Banner
    - Added A Generic Hero Banner: HeroBanner.vue
    - Changed AboutHero.vue to use the new component

---
+++ components/HeroBanner.vue
@@ -0,0 +1,17 @@
+<template>
+    <div
+    class="md:w-full flex bg-[url('~/assets/heroTexture.png')] bg-cover bg-no-repeat bg-center justify-center items-center font-josefin text-white font-bold uppercase"
+    >
+        <div class="text-3xl sm:text-6xl p-10">{{text}}</div>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
