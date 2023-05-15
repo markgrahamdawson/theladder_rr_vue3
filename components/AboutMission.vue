f5620a92dabd2391767dcac1aba3c95f39773306
markgrahamdawson <markgrahamdawson@gmail.com>
2023-05-15 07:59:39 +0100

    updates to about ourstory component

--- components/AboutMission.vue
+++ components/AboutMission.vue
@@ -2,12 +2,12 @@
   <div class="bg-white md:flex w-full lg:h-[600px]">
     <!-- Left Col -->
     <div
-      class="bg-offwhite text-left px-12 py-20 md:w-1/3 md:my-10 text-sm md:text-base"
+      class="md:text-left bg-offwhite text-center px-12 py-20 md:w-1/3 md:my-10 md:text-base"
     >
       <h1 class="title font-josefin font-semibold text-4xl title pb-4">
         Mission
       </h1>
-      <div class="font-semibold">
+      <div class="font-semibold font-josefin">
         We are building a creative space for Redruth and Cornwall. This will
         include space for creatives to work including studios and co-working but
         also for community events and activities including fance classes,
@@ -22,7 +22,7 @@
       class="object-cover md:w-2/3"
     />
     <IconsDrops
-      class="fill-white absolute h-40 w-40 md:h-60 md:w-60 right-[40px] -bottom-[50px] sm:-bottom-[120px] md:-bottom-[130px] lg:bottom-[10px]"
+      class="fill-white absolute h-40 w-40 sm:h-60 sm:w-60 right-[40px] -bottom-[150px] sm:-bottom-[300px] md:-bottom-[130px] lg:bottom-[10px]"
     />
     <img />
   </div>
--- components/AboutOurStory.vue
+++ components/AboutOurStory.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="flex flex-col-reverse md:flex-row">
     <!-- left box -->
-    <div class="md:w-1/2 m-auto flex-col">
+    <div class="relative md:w-1/2 m-auto flex-col">
       <img
         src="~/assets/ourStory.png"
         alt="Leonora and Live standing in front of The Ladder Logo"
       />
 
       <IconsSquigles
-        class="absolute fill-teal w-1/6 rotate-90 left-[-30px] bottom-0 md:block sm:hidden"
+        class="absolute fill-teal rotate-90 h-40 w-40 xl:h-60 xl:w-60 -bottom-[20px] -left-[10px] sm:-bottom-[20px] sm:left-[10px]"
       />
-      <IconsSquigles
-        class="absolute fill-teal h-44 w-44 lg:h-64 lg:w-64 rotate-[270deg] left-1/2 translate-x-[-250px] top-[60px] md:block sm:hidden"
+      <IconsSquigles2
+        class="absolute fill-teal h-40 w-40 xl:h-60 xl:w-60 rotate-[270deg] translate-x-[-250px] z-50 -top-[60px] -right-[250px] sm:-top-[60px] sm:-right-[250px] xl:-top-[90px] xl:-right-[250px]"
       />
     </div>
     <!-- right box -->
     <div
-      class="md:w-1/2 md:text-left m-auto bg-offwhite font-josefin md:my-4 px-12 py-20 space-y-4 relative sm:w-full sm:text-center"
+      class="md:w-1/2 md:text-left m-auto bg-offwhite font-josefin md:my-4 px-12 py-20 space-y-4 relative sm:w-full text-center"
     >
       <h1 class="font-semibold text-3xl">Our Story</h1>
       <div class="font-semibold">
