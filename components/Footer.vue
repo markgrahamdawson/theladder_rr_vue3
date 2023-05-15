de418f12562352f3359e8e9cad65d227d19c89a4
Richie <imarichicken@gmail.com>
2023-05-14 18:59:38 -0700

    Updated Footed to new Design

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,18 +1,27 @@
 <template>
-<div class="bg-white px-8 pt-10 pb-20 md:px-20 border-t-4 flex">
-  <div class="space-y-2 font-josan grow  text-sm">
+<div class=" relative bg-contain bg-no-repeat bg-center px-8 pt-10 pb-20 md:px-20 border-t-4 flex">
+
+  <div class="absolute -z-10 top-[100px]"><img src="~/assets/rainOnArt.png" alt=""></div>
+
+  <!-- Left -->
+  <div class="space-y-2 font-josan text-sm bg-white h-auto">
     <div class="uppercase font-bold">
       Contact
     </div>
-    <div>
+    <div class="font-semibold">
       <p>Redruth Former Library Cic,</p>
       <p>ClintonRd,</p>
       <p>Redruth, Cornwall,</p>
       <p>TR4 7PH</p>
     </div>
-    <div>tel: 0(+44)779443040</div>
+    <div class="font-semibold">tel: 0(+44)779443040</div>
   </div>
-  <div>
+
+  <!-- Center -->
+  <div class="grow"></div>
+
+  <!-- Right -->
+  <div class="bg-white">
     <div class="flex space-x-2 pt-2">
       <a class="hover:fill-gray-300" href="https://www.instagram.com/theladder_rr/"><IconsInstagram/></a>
       <a class="hover:fill-gray-300" href=""><IconsFacebook/></a>
