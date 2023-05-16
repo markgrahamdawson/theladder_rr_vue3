5b6a11628cbbc9acb51502a905ebc667eb06286c
markgrahamdawson <markgrahamdawson@gmail.com>
2023-05-16 19:08:23 +0100

    small changes to style

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,36 +1,45 @@
 <template>
-<div class=" relative bg-contain bg-no-repeat bg-center px-8 pt-10 pb-20 md:px-20 border-t-4 flex">
-
-  <div class="absolute -z-10 top-[100px]"><img src="~/assets/rainOnArt.png" alt=""></div>
-
-  <!-- Left -->
-  <div class="space-y-2 font-josan text-sm bg-white h-auto">
-    <div class="uppercase font-bold">
-      Contact
+  <div
+    class="relative bg-contain bg-no-repeat bg-center px-8 pt-10 pb-20 md:px-20 border-t-4 flex"
+  >
+    <div class="absolute -z-10 top-[100px] sm:top-[100px]">
+      <img src="~/assets/rainOnArt.png" alt="" />
     </div>
-    <div class="font-semibold">
-      <p>Redruth Former Library Cic,</p>
-      <p>ClintonRd,</p>
-      <p>Redruth, Cornwall,</p>
-      <p>TR4 7PH</p>
+
+    <!-- Left -->
+    <div class="space-y-2 font-josan text-xs sm:text-sm xl:text-lg h-auto">
+      <div class="uppercase font-bold">Contact</div>
+      <div class="font-semibold">
+        <p>Redruth Former Library Cic,</p>
+        <p>ClintonRd,</p>
+        <p>Redruth, Cornwall,</p>
+        <p>TR4 7PH</p>
+      </div>
+      <div class="font-semibold">tel: 0(+44)779443040</div>
     </div>
-    <div class="font-semibold">tel: 0(+44)779443040</div>
+
+    <!-- Center -->
+    <div class="grow"></div>
+
+    <!-- Right -->
+    <div class="">
+      <div class="flex space-x-2 pt-2">
+        <a
+          class="hover:fill-gray-300"
+          href="https://www.instagram.com/theladder_rr/"
+          ><IconsInstagram class="h-6 w-6 md:h-8 md:w-8"
+        /></a>
+        <a class="hover:fill-gray-300" href=""
+          ><IconsFacebook class="h-6 w-6 md:h-8 md:w-8"
+        /></a>
+        <a
+          class="hover:fill-gray-300"
+          href="https://mobile.twitter.com/riftexcursions"
+          ><IconsTwitter class="h-6 w-6 md:h-8 md:w-8"
+        /></a>
+      </div>
+    </div>
   </div>
-
-  <!-- Center -->
-  <div class="grow"></div>
-
-  <!-- Right -->
-  <div class="bg-white">
-    <div class="flex space-x-2 pt-2">
-      <a class="hover:fill-gray-300" href="https://www.instagram.com/theladder_rr/"><IconsInstagram/></a>
-      <a class="hover:fill-gray-300" href=""><IconsFacebook/></a>
-      <a class="hover:fill-gray-300" href="https://mobile.twitter.com/riftexcursions"><IconsTwitter/></a>
-    </div>
-  </div>
-</div>
 </template>
 
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
