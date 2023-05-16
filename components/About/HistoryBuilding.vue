fad6363c1bebfcef0ec35b5520fdce2ed74f177c
markgrahamdawson <markgrahamdawson@gmail.com>
2023-05-16 21:24:41 +0100

    added jpe section to about history

--- components/About/HistoryBuilding.vue
+++ components/About/HistoryBuilding.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- top -->
   <div class="py-10 md:py-28 bg-offwhite md:flex items-center">
     <div
       class="md:w-2/3 px-10 md:pl-28 md:pr-12 font-josan font-bold flex flex-col justify-center"
@@ -29,6 +30,38 @@
       <IconsDrops
         class="absolute fill-white -top-[100px] md:-top-[140px] right-0 w-[200px] md:w-[250px]"
       />
+    </div>
+  </div>
+  <!-- bottom -->
+  <div class="pb-10 md:pb-28 bg-offwhite md:flex items-center">
+    <div class="relative md:w-1/3 pt-10 md:pt-0 h-full">
+      <img
+        src="~/assets/images/buildingback.png"
+        alt="A picture of the library building in the snow"
+        class="object-fill grayscale hidden md:block"
+      />
+      <IconsSquigles
+        class="absolute fill-white -top-[100px] md:-top-[140px] right-0 w-[200px] md:w-[250px] hidden md:block"
+      />
+    </div>
+    <div
+      class="md:w-2/3 px-10 md:pl-28 md:pr-12 font-josan font-bold flex flex-col justify-center"
+    >
+      <div>
+        Cornish philanthropist John Passmore Edwards founded th Rdruth Library,
+        now The Ladder, as part of a building programme initiated to deal with
+        unemployment in 1895 after the copper mining industry rapidly declined
+        in Redruth. He bequeathed £2,000 for the construction of the library to
+        match Octavius Allen Ferris of Truro's sum for the provision of free
+        libraries. "There was no reason why libraries", he said, "should not be
+        universal - in every town, many, and in every village, one." The
+        building was designed by Redruth architect James Hicks, more than 7,000
+        people gathered witness Passmore Edwards lay the foundation stone, a
+        day's holiday was declared, and the towns people celebrated with
+        carnival and fireworks. The Passmore Edwards Free Library is a symbol of
+        education, hope, nurture, and opportunities. All twenty of the Passmore
+        Edwards buildings in cornwall have survived.
+      </div>
     </div>
   </div>
 </template>
