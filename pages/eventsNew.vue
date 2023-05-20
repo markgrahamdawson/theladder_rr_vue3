8940b2897f231d1b865d18f91130a14fdf995ad6
markgrahamdawson <markgrahamdawson@gmail.com>
2023-05-20 18:28:36 +0100

    built out events page functionality

--- pages/eventsNew.vue
+++ pages/eventsNew.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div class="min-h-screen md:p-16 space-y-4">
-      <EventsUpcoming />
+      <!-- <EventsUpcoming /> -->
+      <StoryblokComponent
+        v-if="story"
+        :blok="story.content"
+      ></StoryblokComponent>
       <EventsPast />
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const resolveRelations = ["FeaturedEvents.events"];
+
+const story = await useAsyncStoryblok(
+  "events",
+  {
+    version: useRoute().query._storyblok ? "draft" : "published",
+    resolve_relations: resolveRelations,
+  },
+  {
+    resolveRelations,
+  }
+);
+</script>
