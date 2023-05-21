2f4ac16dafd0d4cf508f546e2cc8164dc3c71b1e
markgrahamdawson <markgrahamdawson@gmail.com>
2023-05-21 20:22:39 +0100

    started adding past events card

--- components/Events/Past.vue
+++ components/Events/Past.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="pb-6">
+  <div class="pb-6 pt-16">
     <div class="font-josan pb-4 text-xl">Past Events</div>
-    <div class="flex space-x-2">
-      <div class="border w-1/3 h-[110px]">event 1</div>
-      <div class="border w-1/3">event 2</div>
-      <div class="border w-1/3">event 3</div>
+    <div class="sm:flex sm:space-x-2">
+      <div class="border sm:w-1/3 h-[110px]">event 1</div>
+      <div class="border sm:w-1/3">event 2</div>
+      <div class="border sm:w-1/3">event 3</div>
     </div>
   </div>
 </template>
