1cc13a1604c987890745fa620fbd42220016c07e
markgrahamdawson <markgrahamdawson@gmail.com>
2023-05-31 20:08:33 +0100

    added smaller images

--- components/ComingSoon.vue
+++ components/ComingSoon.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center items-center p-[200px]">
     <div class="font-josan text-5xl">Coming soon...</div>
-    <IconsSnake class="h-[200px]" />
+    <IconsSnake class="h-[200px] w-[200px]" />
   </div>
 </template>
