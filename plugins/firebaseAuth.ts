2c28bc596df40dc56aef485c436e1cd1a076d3ae
Mark Dawson <markgrahamdawson@gmail.com>
2023-09-22 06:32:13 +0100

    removed firebase auth

--- plugins/firebaseAuth.ts
+++ plugins/firebaseAuth.ts
@@ -4,24 +4,24 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, on
 import { getAnalytics } from "firebase/analytics";
 
 export default defineNuxtPlugin(nuxtApp => {
-    const config = useRuntimeConfig()
-    // Doing something with nuxtApp
-    const firebaseConfig = {
-        apiKey: config.FIREBASE_API_KEY,
-        authDomain: config.FIREBASE_AUTH_DOMAIN,
-        projectId: config.FIREBASE_PROJECT_ID,
-        storageBucket: config.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
-        appId: config.FIREBASE_APP_ID,
-        measurementId: config.FIREBASE_MEASUREMENT_ID
-    };
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
-    // console.log(app)
-    initUser();
+    // const config = useRuntimeConfig()
+    // // Doing something with nuxtApp
+    // const firebaseConfig = {
+    //     apiKey: config.FIREBASE_API_KEY,
+    //     authDomain: config.FIREBASE_AUTH_DOMAIN,
+    //     projectId: config.FIREBASE_PROJECT_ID,
+    //     storageBucket: config.FIREBASE_STORAGE_BUCKET,
+    //     messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
+    //     appId: config.FIREBASE_APP_ID,
+    //     measurementId: config.FIREBASE_MEASUREMENT_ID
+    // };
+    // const app = initializeApp(firebaseConfig);
+    // const analytics = getAnalytics(app);
+    // // console.log(app)
+    // initUser();
 
-    const auth = getAuth()
-    nuxtApp.vueApp.provide('auth', auth)
-    nuxtApp.provide('auth', auth)
+    // const auth = getAuth()
+    // nuxtApp.vueApp.provide('auth', auth)
+    // nuxtApp.provide('auth', auth)
 
 })
