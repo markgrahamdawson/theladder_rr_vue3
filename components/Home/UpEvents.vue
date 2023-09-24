14c823a2a570739ec91f11508c460943426b0d4d
Mark Dawson <markgrahamdawson@gmail.com>
2023-09-24 12:19:01 +0100

    Updated home page event

--- components/Home/UpEvents.vue
+++ components/Home/UpEvents.vue
@@ -5,14 +5,13 @@
     <!-- Left Col -->
     <div class="relative md:w-1/3 bg-black flex flex-col justify-center">
       <div class="text-white font-josefin mx-auto space-y-5 p-14 text-center">
-        <div class="uppercase text-4xl font-semibold">Eyes Peeled</div>
-        <div class="font-semibold">22nd September</div>
+        <div class="uppercase text-4xl font-semibold">Shallal Zine Launch</div>
+        <div class="font-semibold">14th October</div>
         <div>
-          EYES PEELED are coming to The Ladder! The Darkroom in The Ladder presents
-          A Girl Walks Home Alone at Night.
+          Free screen printing workshop, community poster exhibition and zine launch. Plus cake! All welcome :)
         </div>
         <div>
-          <a href="https://www.eventbrite.co.uk/e/eyes-peeled-film-night-a-girl-walks-home-alone-at-night-tickets-717038169467#:~:text=Eyes%20Peeled%20is%20a%20monthly,Vivian%20Almas%20with%20a%20Q%26A.">
+          <a href="https://www.eventbrite.co.uk/e/shallal-zine-launch-tickets-722706744327?aff=oddtdtcreator">
             <button
               class="bg-orange text-white font-semibold rounded-md px-5 py-2 uppercase"
             >
@@ -26,8 +25,8 @@
     <nuxt-img 
         quality="80"
         format="webp" 
-        src="https://a.storyblok.com/f/181301/1280x720/3a5ee0b7fa/agirl.jpg"
+        src="https://a.storyblok.com/f/181301/3508x4961/e1673f5403/shallal-zine-launch-poster-jpeg.jpeg"
         alt="Still from movie"
-        class="h-[150px] md:h-auto md:w-2/3 object-cover object-center max-h-[400px] md:m-auto"/>
+        class="h-[150px] md:h-auto md:w-2/3 object-cover object-top max-h-[400px] md:m-auto"/>
   </div>
 </template>
