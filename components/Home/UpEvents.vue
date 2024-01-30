fc06ee6763c8a26c2a81e4017407943bfef1e936
Mark Dawson <markgrahamdawson@gmail.com>
2024-01-30 20:43:57 +0000

    Made homeEvents work with storyblok

--- components/Home/UpEvents.vue
+++ components/Home/UpEvents.vue
@@ -5,13 +5,13 @@
     <!-- Left Col -->
     <div class="relative md:w-1/3 bg-black flex flex-col justify-center">
       <div class="text-white font-josefin mx-auto space-y-5 p-14 text-center">
-        <div class="uppercase text-4xl font-semibold">Shallal Zine Launch</div>
-        <div class="font-semibold">14th October</div>
+        <div class="uppercase text-4xl font-semibold">{{ blok.events.content.performance }}</div>
+        <div class="font-semibold">{{ getDate(blok.events.content.date) }}</div>
         <div>
-          Free screen printing workshop, community poster exhibition and zine launch. Plus cake! All welcome :)
+          {{ blok.events.content.shortDescription }}
         </div>
         <div>
-          <a href="https://www.eventbrite.co.uk/e/shallal-zine-launch-tickets-722706744327?aff=oddtdtcreator">
+          <a :href=blok.events.content.eventbright>
             <button
               class="bg-orange text-white font-semibold rounded-md px-5 py-2 uppercase"
             >
@@ -25,8 +25,31 @@
     <nuxt-img 
         quality="80"
         format="webp" 
-        src="https://a.storyblok.com/f/181301/3508x4961/e1673f5403/shallal-zine-launch-poster-jpeg.jpeg"
+        :src=blok.events.content.thumbnail.filename
         alt="Still from movie"
         class="h-[150px] md:h-auto md:w-2/3 object-cover object-top max-h-[400px] md:m-auto"/>
   </div>
 </template>
+
+<script lang="ts" setup>
+
+const props = defineProps({
+  blok: Object,
+});
+
+function getDate(datetimeStr) {
+  // Create a new Date object from the datetime string
+  var dateObj = new Date(datetimeStr);
+
+  // Get the day, month, and year components from the date object
+  var day = String(dateObj.getDate()).padStart(2, "0");
+  var month = String(dateObj.getMonth() + 1).padStart(2, "0");
+  var year = String(dateObj.getFullYear());
+
+  // Reformat the date as 'DD/MM/YYYY'
+  var formattedDate = `${day}/${month}/${year}`;
+
+  return formattedDate;
+}
+
+</script>
